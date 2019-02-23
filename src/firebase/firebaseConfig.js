7dd4229882635ebd5a070a38be5a8d2f98c920af
neil aldrei <neilaldrei@icloud.com>
2019-02-24 07:47:20 +0800

    Added firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyANTpfVAwrqhpClJgw5oSVZGpq6Sal8SPY",
+    authDomain: "cbsua-cvm-poll.firebaseapp.com",
+    databaseURL: "https://cbsua-cvm-poll.firebaseio.com",
+    projectId: "cbsua-cvm-poll",
+    storageBucket: "cbsua-cvm-poll.appspot.com",
+    messagingSenderId: "1031679522788"
+}
---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import firebaseConfig from "./firebaseConfig";
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
+
+const firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
