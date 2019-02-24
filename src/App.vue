146799c05536754cd1a9d0a2e79d4b26fed36602
neil aldrei <neilaldrei@icloud.com>
2019-02-24 14:50:58 +0800

    Added Rules

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,28 @@
   <div id="app">
     <app-signin></app-signin>
     <router-view />
+    <div class="description">
+      <ul>
+        <lh>
+          <h4>Rules</h4>
+        </lh>
+        <li>
+          Voters must connect to facebook before proceeding.
+        </li>
+        <li>
+          Voters will get four(4) Maximum total votes. Two(2) for Mr. VetMed candidates and Two(2) for Ms. VetMed Candidates.
+        </li>
+        <li>
+          Voters cannot vote for the same candidate twice.
+        </li>
+        <li>
+          Voters cannot vote if their total votes reaches zero(0).
+        </li>
+        <li>
+          Mr. and Ms. VetMed poll will end on <b>February 28, 2019 @12pm</b>
+        </li>
+      </ul>
+    </div>
     <app-footer></app-footer>
   </div>
 </template>
